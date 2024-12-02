bdf68831cb32cf9d8652a9a3e9ab6a49e5555632
Muhammet Soil <90849828+MuhammetSoil@users.noreply.github.com>
2024-12-02 19:02:57 +0300

    Update and rename README.md to README.sh

---
+++ README.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# Güncelleme ve Yükseltme
+sudo apt update
+sudo apt upgrade -y
+
+# Swap Kapatma
+sudo swapoff -a
+sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+
+# Containerd için modüllerin yüklenmesi
+sudo tee /etc/modules-load.d/containerd.conf <<EOF
+overlay
+br_netfilter
+EOF
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+# Kubernetes için sysctl ayarları
+sudo tee /etc/sysctl.d/kubernetes.conf <<EOF
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
+EOF
+
+sudo sysctl --system
+
+# Gerekli paketlerin kurulması
+sudo apt install -y curl gnupg2 software-properties-common apt-transport-https ca-certificates
+
+# Docker GPG anahtarını ekleme ve repository ekleme
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/docker.gpg
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+# Docker ve Containerd kurulumu
+sudo apt update
+sudo apt install -y containerd.io
+containerd config default | sudo tee /etc/containerd/config.toml >/dev/null 2>&1
+sudo sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
+sudo systemctl restart containerd
+sudo systemctl enable containerd
+
+# Kubernetes repository ve anahtarları
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+
+# Kubernetes paketlerinin kurulumu
+sudo apt update
+sudo apt install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+
+# Kubernetes Master node kurulumu
+sudo kubeadm init
+
+# Kubernetes yapılandırma dosyasını kopyalama
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+# Kubernetes node'ları kontrol etme
+kubectl get nodes
+
+# Nginx pod'u başlatma
+kubectl run nginx --image=nginx
