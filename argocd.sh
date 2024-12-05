4a5f87d716b930e1889cf5d9afb8c22922db250e
Muhammet Soil <toprakmsadullah@gmail.com>
2024-12-05 15:18:01 +0300

    Update argocd.sh

---
+++ argocd.sh
@@ -0,0 +1,8 @@
+# Argo Cd Kurulum
+
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "NodePort", "ports": [{"port": 80, "targetPort": 8080, "nodePort": 30007}]}}'
+
+kubectl get svc -n argocd
